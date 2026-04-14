385a29ed737f4c314fd46299a9a7427c911ad1d7
nicom06 <119193168+nicom06@users.noreply.github.com>
2026-04-13 22:22:12 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import libreria_funciones as lf
+
+st.title("Mi primera app")
+
+st.sidebar.title("Datos")
+st.image("logo.png",width=100)
+st.sidebar.image("logodmc.png")
+st.title("clase 5 funciones")
+
+p=st.number_input("ingrese el monto principal",value=12000)
+t=st.number_input("ingrese la tasa anual",value=0.05)
+a=st.slider("Seleccione el numero de años del prestamo",min_value=1,max_value=5)
+pa=st.number_input("Cantidad de pagos por años",value=12)
+cuota=lf.cuota_prestamo(p,t,a,pa)
+st.write(cuota)
